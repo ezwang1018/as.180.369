8defe9f77d5d9a3575eb89d6f3b6281a9aefaea9
Adrian Monninger <66739352+AMonninger@users.noreply.github.com>
2024-12-02 19:57:00 +0100

    Create environment in reproduce.sh

--- contrib/AMonninger/Paper_Restructured/reproduce.sh
+++ contrib/AMonninger/Paper_Restructured/reproduce.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Create the environment
+ml anaconda
+conda env update --file ./binder/environment.yml
+conda activate ./econark
+    
 # Run the Jupyter notebook and execute all cells in place
 python -m ipykernel install --user --name econark
 jupyter nbconvert --to notebook --execute --inplace 04_dataanalysis.ipynb
